12dfa93b41fc7968622dc66d658df962099ab051
Bri <bpopson@gmail.com>
2016-03-18 08:44:30 -0700

    do initial set up of brower.js and github.js files

---
+++ js/browser.js
@@ -0,0 +1,4 @@
+var callMap = require("./../js/map.js").initMap;
+
+$( document ).ready(function() {
+  google.maps.event.addDomListener(window, 'load', callMap);
---
+++ js/gitHub.js
@@ -0,0 +1,1 @@
+var apiKey = "071e8d6d83d8d819d1d102c4c4864aed65a9c477";
