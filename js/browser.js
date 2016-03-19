b1956ad18e3006518b858d3e6ae15ff997ae2818
Bri <bpopson@gmail.com>
2016-03-18 20:18:15 -0700

    generate new ApiKey

--- js/browser.js
+++ js/browser.js
@@ -2,6 +2,8 @@ var getRepos = require('./../js/gitHub.js').getRepos;
 var getUser = require('./../js/gitHub.js').getUser;
 var apiKey = require('./../.env').apiKey;
 
+console.log(apiKey);
+
 $( document ).ready(function() {
   $('form#search').submit(function(event){
     event.preventDefault();
